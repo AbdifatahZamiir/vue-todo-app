c3605fa6886ebde0800aa7cfc8373de4348bf061
abdifatah <abdifatahzammiir@gmail.com>
2020-02-28 19:22:55 +0300

    add files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="text-center">
+    <scoreCounter :msg="msg" />
+  </div>
+</template>
+
+<script>
+import ScoreCounter from "./components/scoreCounter.vue";
+
+export default {
+  name: "App",
+  components: {
+    ScoreCounter
+  },
+  data() {
+    return {
+      msg: "Player Score"
+    };
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  color: blue;
+}
+</style>
---
+++ src/components/scoreCounter.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello text-center">
+    <h1>{{ msg }}</h1>
+    <div class="card">
+      <div class="card-body">
+        <h3>Player 1</h3>
+        <button class="btn btn-success mr-3" @click="increaseCounter(player_1)">Up</button>
+        <span>{{ player_1.score }}</span>
+        <button class="btn btn-danger ml-3" @click="dencreaseCounter(player_1)">Down</button>
+      </div>
+      <div class="card-body">
+        <h3>Player 2</h3>
+        <button class="btn btn-success mr-3" @click="increaseCounter(player_2)">Up</button>
+        <span>{{ player_2.score }}</span>
+        <button class="btn btn-danger ml-3" @click="dencreaseCounter(player_2)">Down</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "scoreCounter",
+  props: {
+    msg: String,
+    color: String
+  },
+  data() {
+    return {
+      player_1: {
+        score: 0
+      },
+      player_2: {
+        score: 0
+      }
+    };
+  },
+  methods: {
+    increaseCounter(player) {
+      player.score += 1;
+    },
+    dencreaseCounter(player) {
+      if (player.score > 0) {
+        player.score -= 1;
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.green {
+  color: green;
+}
+</style>
